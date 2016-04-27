f3f25bfff3eff0a07a7d6e211c45a6a51c7249a9
Maxi Ferreira <charca@gmail.com>
2016-04-27 16:00:14 -0700

    Implemented echoing bot, moved server to express

--- index.js
+++ index.js
@@ -1,38 +1,56 @@
 'use strict';
 
-const Hapi = require('hapi');
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 const keys = require('./.keys/facebook');
 
-const server = new Hapi.Server();
-server.connection({
-  host: '0.0.0.0',
-  port: 8123
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/bot/messenger/v1/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === keys['verify_token']) {
+    return res.send(req.query['hub.challenge']);
+  }
+  return res.send('Error, wrong validation token');
 });
 
-server.route({
-  method: 'GET',
-  path: '/bot/messenger/v1/webhook',
-  handler: function(request, reply) {
-    if (request.query['hub.verify_token'] === keys['verify_token']) {
-      return reply(request.query['hub.challenge']);
+app.post('/bot/messenger/v1/webhook', function (req, res) {
+  const messaging_events = req.body.entry[0].messaging;
+  for (let i = 0; i < messaging_events.length; i++) {
+    const event = req.body.entry[0].messaging[i];
+    const sender = event.sender.id;
+    if (event.message && event.message.text) {
+      var text = event.message.text;
+      sendTextMessage(sender, 'Text received, echo: ' + text.substring(0, 200));
     }
-    return reply('Error, wrong validation token');
   }
+  return res.sendStatus(200);
 });
 
-server.route({
-  method: 'POST',
-  path: '/bot/messenger/v1/webhook',
-  handler: function(request, reply) {
-    reply({
-      response: 'from POST'
-    });
-  }
-});
+function sendTextMessage(sender, text) {
+  const messageData = {
+    text: text
+  };
 
-server.start((err) => {
-  if (err) {
-      throw err;
-  }
-  console.log('Server running at:', server.info.uri);
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: keys['access_token'] },
+    method: 'POST',
+    json: {
+      recipient: { id: sender },
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
+app.listen(8123, '0.0.0.0', function () {
+  console.log('Facebook Messenger bot started on port 8123');
 });
