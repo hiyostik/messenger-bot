8c63a111fab1582fc7bc2e4b1618bcd497d1c266
Maxi Ferreira <charca@gmail.com>
2016-04-27 19:30:38 -0700

    Moved facebook api methods to facebook.js, fixed platform lookup

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
 const keys = require('./.keys/facebook');
-
+const facebook = require('./src/facebook');
 const messages = require('./src/constants/messages');
 
 const app = express();
@@ -23,12 +22,16 @@ app.post('/bot/messenger/v1/webhook', function (req, res) {
   for (let i = 0; i < messaging_events.length; i++) {
     const event = req.body.entry[0].messaging[i];
     const sender = event.sender.id;
+    const config = {
+      sender: sender,
+      access_token: keys.access_token
+    };
     if (event.message && event.message.text) {
-      parseQuery(event.message.text, sender);
+      parseQuery(event.message.text, config);
     }
     if (event.postback) {
       const text = JSON.stringify(event.postback);
-      sendTextMessage(sender, 'Postback received: ' + text.substring(0, 200));
+      facebook.sendTextMessage(config, 'Postback received: ' + text.substring(0, 200));
     }
   }
   return res.sendStatus(200);
@@ -46,10 +49,10 @@ const platformsIdMap = {
 
 function getSearchObject(query) {
   for (var key in platformsMap) {
-    if (query.indexOf(key) !== -1) {
+    if (query.indexOf(key.toLowerCase()) !== -1) {
       return {
-        game: query.replace(new RegExp(`(for )?${key}`), ''),
-        platform: key
+        game: query.replace(new RegExp(`(for )?${key}`, 'i'), ''),
+        platform: platformsMap[key]
       }
     }
   }
@@ -68,15 +71,16 @@ function parseSearchGame(query) {
   return false;
 }
 
-function parseQuery(query, sender) {
+function parseQuery(query, config) {
   // Search Game
   const searchObject = parseSearchGame(query);
   if (searchObject !== false) {
     // TODO: Request search
     const results = [];
     if (results.length > 0) {
-      sendTextMessage(sender, 'found something!');
+      facebook.sendTextMessage(config, 'found something!');
     } else {
+      console.log(searchObject);
       if(searchObject.platform) {
         const button = {
           'type': 'postback',
@@ -86,90 +90,16 @@ function parseQuery(query, sender) {
             'query': searchObject.game
           }
         };
-        sendButtonMessage(sender, messages.GAME_NOT_FOUND + searchObject.game + ' for ' + platformsIdMap[searchObject.platform], [button]);
+        facebook.sendButtonMessage(config, messages.GAME_NOT_FOUND + searchObject.game + ' for ' + platformsIdMap[searchObject.platform], [button]);
       } else {
-        sendTextMessage(sender, messages.GAME_NOT_FOUND);
+        facebook.sendTextMessage(config, messages.GAME_NOT_FOUND + searchObject.game);
       }
     }
   } else {
     // TODO: Search if Query is Game
-    sendTextMessage(sender, messages.UNRECOGNIZED_QUERY);
+    facebook.sendTextMessage(config, messages.UNRECOGNIZED_QUERY);
   }
 };
-
-function sendTextMessage(sender, text) {
-  const messageData = {
-    text: text
-  };
-  sendMessage(sender, messageData);
-}
-
-function sendButtonMessage(sender, text, buttons) {
-  const messageData = {
-    'attachment': {
-      'type': 'template',
-      'payload': {
-        'template_type': 'button',
-        'text': text,
-        'buttons': buttons
-      }
-    }
-  };
-  sendMessage(sender, messageData);
-}
-
-function sendGenericTemplate(sender) {
-  const messageData = {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "generic",
-        "elements": [{
-          "title": "First card",
-          "subtitle": "Element #1 of an hscroll",
-          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-          "buttons": [{
-            "type": "web_url",
-            "url": "https://www.messenger.com/",
-            "title": "Web url"
-          }, {
-            "type": "postback",
-            "title": "Postback",
-            "payload": "Payload for first element in a generic bubble",
-          }],
-        },{
-          "title": "Second card",
-          "subtitle": "Element #2 of an hscroll",
-          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-          "buttons": [{
-            "type": "postback",
-            "title": "Postback",
-            "payload": "Payload for second element in a generic bubble",
-          }],
-        }]
-      }
-    }
-  };
-  sendMessage(sender, messageData);
-}
-
-function sendMessage(sender, messageData) {
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: keys['access_token'] },
-    method: 'POST',
-    json: {
-      recipient: { id: sender },
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
-}
 
 app.listen(8123, '0.0.0.0', function () {
   console.log('Facebook Messenger bot started on port 8123');
---
+++ src/facebook.js
@@ -0,0 +1,82 @@
+const request = require('request');
+
+function sendTextMessage(config, text) {
+  const messageData = {
+    text: text
+  };
+  sendMessage(config, messageData);
+}
+
+function sendButtonMessage(config, text, buttons) {
+  const messageData = {
+    'attachment': {
+      'type': 'template',
+      'payload': {
+        'template_type': 'button',
+        'text': text,
+        'buttons': buttons
+      }
+    }
+  };
+  sendMessage(config, messageData);
+}
+
+function sendGenericTemplate(config) {
+  const messageData = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [{
+          "title": "First card",
+          "subtitle": "Element #1 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+          "buttons": [{
+            "type": "web_url",
+            "url": "https://www.messenger.com/",
+            "title": "Web url"
+          }, {
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for first element in a generic bubble",
+          }],
+        },{
+          "title": "Second card",
+          "subtitle": "Element #2 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+          "buttons": [{
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for second element in a generic bubble",
+          }],
+        }]
+      }
+    }
+  };
+  sendMessage(config, messageData);
+}
+
+function sendMessage(config, messageData) {
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: config['access_token'] },
+    method: 'POST',
+    json: {
+      recipient: { id: config.id },
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
+module.exports = {
+  sendTextMessage: sendTextMessage,
+  sendButtonMessage: sendButtonMessage,
+  sendGenericTemplate: sendGenericTemplate,
+  sendMessage: sendMessage
+};
