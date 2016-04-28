7d376748688f99993e8524cf18a2332f151b4fe2
Maxi Ferreira <charca@gmail.com>
2016-04-27 19:01:23 -0700

    Added basic search

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const keys = require('./.keys/facebook');
 
+const messages = require('./src/constants/messages');
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -22,20 +24,96 @@ app.post('/bot/messenger/v1/webhook', function (req, res) {
     const event = req.body.entry[0].messaging[i];
     const sender = event.sender.id;
     if (event.message && event.message.text) {
-      var text = event.message.text;
-      if (text === 'generic') {
-        sendGenericTemplate(sender);
-      } else {
-        sendTextMessage(sender, 'Text received, echo: ' + text.substring(0, 200));
-      }
+      parseQuery(event.message.text, sender);
+    }
+    if (event.postback) {
+      const text = JSON.stringify(event.postback);
+      sendTextMessage(sender, 'Postback received: ' + text.substring(0, 200));
     }
   }
   return res.sendStatus(200);
 });
 
+const platformsMap = {
+  'PS4': 1,
+  'PS3': 2,
+};
+
+const platformsIdMap = {
+  1: 'PS4',
+  2: 'PS3'
+};
+
+function getSearchObject(query) {
+  for (var key in platformsMap) {
+    if (query.indexOf(key) !== -1) {
+      return {
+        game: query.replace(new RegExp(`(for )?${key}`), ''),
+        platform: key
+      }
+    }
+  }
+  return {
+    game: query,
+    platform: false
+  };
+}
+
+function parseSearchGame(query) {
+  const re = /(search (for)?|look (for|up)?|find (me)?)(.*)/i;
+  const match = query.match(re);
+  if (match !== null && match[5]) {
+    return getSearchObject(match[5].toLowerCase().trim());
+  }
+  return false;
+}
+
+function parseQuery(query, sender) {
+  // Search Game
+  const searchObject = parseSearchGame(query);
+  if (searchObject !== false) {
+    // TODO: Request search
+    const results = [];
+    if (results.length > 0) {
+      sendTextMessage(sender, 'found something!');
+    } else {
+      if(searchObject.platform) {
+        const button = {
+          'type': 'postback',
+          'title': 'Try other platform',
+          'payload': {
+            'type': 'RETRY_SEARCH',
+            'query': searchObject.game
+          }
+        };
+        sendButtonMessage(sender, messages.GAME_NOT_FOUND + searchObject.game + ' for ' + platformsIdMap[searchObject.platform], [button]);
+      } else {
+        sendTextMessage(sender, messages.GAME_NOT_FOUND);
+      }
+    }
+  } else {
+    // TODO: Search if Query is Game
+    sendTextMessage(sender, messages.UNRECOGNIZED_QUERY);
+  }
+};
+
 function sendTextMessage(sender, text) {
   const messageData = {
     text: text
+  };
+  sendMessage(sender, messageData);
+}
+
+function sendButtonMessage(sender, text, buttons) {
+  const messageData = {
+    'attachment': {
+      'type': 'template',
+      'payload': {
+        'template_type': 'button',
+        'text': text,
+        'buttons': buttons
+      }
+    }
   };
   sendMessage(sender, messageData);
 }
---
+++ src/constants/messages.js
@@ -0,0 +1,4 @@
+module.exports = {
+  UNRECOGNIZED_QUERY: 'Sorry, I don\'t understand your message, can you try something else? Type HELP if you want to see what I can do for you.',
+  GAME_NOT_FOUND: 'I couldn\'t find any deals for: '
+};
